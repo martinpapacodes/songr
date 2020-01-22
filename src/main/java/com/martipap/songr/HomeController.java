fd1356571a3034f2fa863bde74dfd86c7d3b8259
Martin <martin.papa@outlook.com>
2020-01-22 13:36:04 -0800

    working in progress

--- src/main/java/com/martipap/songr/HomeController.java
+++ src/main/java/com/martipap/songr/HomeController.java
@@ -1,18 +1,26 @@
 package com.martipap.songr;
 
 import org.graalvm.compiler.lir.StandardOp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
 @GetMapping("/hello")
     public String getHelloWorld(Model model) {
     String greeting = "Hello World!";
-    //m is the hashmap of vars available to thymeleaf
+
     model.addAttribute("hello", greeting);
     return "hello";
 }
@@ -26,20 +34,15 @@ public class HomeController {
 
 @GetMapping("/albums")
     public String getAlbums(Model model) {
+    List<Album> albums = albumRepository.findAll();
 
-
-
-    Album[] albumsArray = new Album[]
-            {new Album("Doggy Style", "Snoop", 12, 3, "https://place-hold.it/300x300&text=AlbumCover&bold&italic"),
-            new Album("West Coast", "Kurupt", 10, 4, "https://place-hold.it/300x300&text=AlbumCover&bold&italic"),
-            new Album("Taylor", "Taylor Swift", 9, 4, "https://place-hold.it/300x300&text=AlbumCover&bold&italic")
-            };
-
-
-    model.addAttribute("albumsList", albumsArray);
+//    Album[] albumsArray = new Album[]
+//            {new Album("Doggy Style", "Snoop", 12, 3, "https://place-hold.it/300x300&text=AlbumCover&bold&italic"),
+//            new Album("West Coast", "Kurupt", 10, 4, "https://place-hold.it/300x300&text=AlbumCover&bold&italic"),
+//            new Album("Taylor", "Taylor Swift", 9, 4, "https://place-hold.it/300x300&text=AlbumCover&bold&italic")
+//            };
+    model.addAttribute("albumsList", albums);
     return "albums";
 }
-
-
 }
 
